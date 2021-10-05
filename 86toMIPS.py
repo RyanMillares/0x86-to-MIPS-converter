797023f6294bf86025b5b62be2b378e2cb87212c
RyanMillares <rmillares@chapman.edu>
2021-10-05 14:23:45 -0700

    first commit

---
+++ 86toMIPS.py
@@ -0,0 +1,17 @@
+
+
+
+def h2b(hexVal):
+    print("est")
+
+def main():
+    while(True):
+        print("Hello, welcome to 0x86 to MIPS Converter!")
+        print("Please input a valid 0x86 assembly instruction in Hex (Include the \"0x\" at the start):")
+        userInput = input()
+
+
+
+
+if __name__ == "__main__":
+    main()
