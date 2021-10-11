cc6a5d9f2d595db39c5c55bcd3a44fa6d09bc94e
RyanMillares <rmillares@chapman.edu>
2021-10-11 11:23:16 -0700

    formatted indents and spaces for future jiphy conversion to javascript, removed unneeded debug statements

--- 86toMIPS.py
+++ 86toMIPS.py
@@ -1,5 +1,6 @@
-
-from typing import Match
+# Converts Hex Assembly Instructions into MIPS Instructions
+# Began: October 5, 2021
+# by Ryan Millares
 
 # Hex Validation
 validChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
@@ -46,21 +47,18 @@ def b2d(binVal):
 def validInput(input):
     res = isinstance(input, str)
     if res:
-      
         if len(input) == 10 and input[0:2] == "0x":
-           
             hexOut = input[2:10]
             for i in range(len(hexOut)):
                 currChar = hexOut[i:(i+1)]
-                if currChar in validChars: #change this to "not in" when done and replace code with else error raise
-                    #Debug line
-                    print(str(currChar) + " was found at index " + str(validChars.index(currChar)))
-                else:             
+                if currChar not in validChars: 
                     raise NameError
+
             return hexOut
             
         else: # string input is not of correct length or incorrectly formatted
             raise ValueError
+
     else: # input is not of type string
         raise TypeError
 
@@ -71,23 +69,22 @@ def getType(chars):
     if opCode == "000000":
         print("R-Type")
         return 0
+
     elif (opCode == "000010" or opCode == "000011"):
         print("j-type")
         return 1
+
     else:
         print("i-type")
         return 2
 
+# returns register name given 5-bit binary value
 def getRegister(binVal):
-    print(binVal)
-    #val = int(b2d(binVal))
-    return "$" + str(registers[binVal])
+    val = int(b2d(binVal))
+    return "$" + str(registers[val])
     
-#print(getRegister("10101"))
-#print(registers[int(b2d("01001"))])
-
+# parses fields for r-type instructions
 def parseRType(binValue):
-    print("r")
     output = []
     opcode = binValue[0:6] #not used in r-type, comment out when finalizing
     rsBin = binValue[6:11]
@@ -95,81 +92,73 @@ def parseRType(binValue):
     rdBin = binValue[16:21]
     saBin = binValue[21:26]
     funcBin = binValue[26:32]
-    print(funcBin)
-
-    # Fetch operation name
-    typeId = str(funcBin[0:2])
+    typeId = str(funcBin[0:2]) # Fetch operation name
     if typeId == "00":
         opname = rType00[b2d(funcBin[2:6])]
+
     elif typeId== "01":
         opname = rType01[b2d(funcBin[2:6])]
+
     elif typeId == "10":
         opname = rType10[b2d(funcBin[2:6])]
+
     else:
         print(typeId)
         opname = "Error: Invalid function bits"
+
     output.append(opname)
 
     # Fetch order of fields and error handle
     func3 = funcBin[0:4]
-    if func3 == "0000":
-        print("rd, rt, sa")
-        output.append(getRegister(b2d(rdBin)))
-        output.append(getRegister(b2d(rtBin)))
-        output.append(getRegister(b2d(saBin)))
+    if func3 == "0000": # rd, rt, sa
+        output.append(getRegister(rdBin))
+        output.append(getRegister(rtBin))
+        output.append(getRegister(saBin))
 
-    elif func3 == "0001":
-        # print("rd, rt, rs")
-        output.append(getRegister(b2d(rdBin)))
-        output.append(getRegister(b2d(rtBin)))
-        output.append(getRegister(b2d(rsBin)))
+    elif func3 == "0001": # rd, rt, rs
+        output.append(getRegister(rdBin))
+        output.append(getRegister(rtBin))
+        output.append(getRegister(rsBin))
 
     elif func3 == "0010":
         print("rs or rd, rs")
-        if funcBin[5:6] == "0":
-            # jr
-            output.append(getRegister(b2d(rsBin)))
-        elif funcBin[5:6] == "1":
-            # jalr
-            output.append(getRegister(b2d(rdBin)))
-            output.append(getRegister(b2d(rsBin)))
+        if funcBin[5:6] == "0": # jr
+            output.append(getRegister(rsBin))
+
+        elif funcBin[5:6] == "1": # jalr
+            output.append(getRegister(rdBin))
+            output.append(getRegister(rsBin))
+
         else:
             print("Error, incorrect binary")
 
     elif func3 == "0011":
         print("no fields, syscall and break")
-    elif func3 == "0100":
-        print("rd, rs alternate one")
-        
+
+    elif func3 == "0100": # rd if even decimal,  rs if odd decimal")
         oddOrEven = b2d(funcBin[4:6])
-        if oddOrEven % 2 == 0:
-            # print("00 or 10")
-            output.append(getRegister(b2d(rdBin)))
-        elif oddOrEven % 2 == 1:
-            # print("01 or 11")
-            output.append(getRegister(b2d(rsBin)))
+        if oddOrEven % 2 == 0: # print("00 or 10")
+            output.append(getRegister(rdBin))
+            
+        elif oddOrEven % 2 == 1: # print("01 or 11")
+            output.append(getRegister(rsBin))
 
-    elif func3 == "0110":
-        print("rs, rt")
-        output.append(getRegister(b2d(rsBin)))
-        output.append(getRegister(b2d(rtBin)))
+    elif func3 == "0110": # rs, rt
+        output.append(getRegister(rsBin))
+        output.append(getRegister(rtBin))
 
-    elif (func3 == "1000" or func3 == "1001") or func3 == "1010":
-        print("rd, rs, rt")
-        print(rdBin)
-        output.append(getRegister(b2d(rdBin)))
-        output.append(getRegister(b2d(rsBin)))
-        output.append(getRegister(b2d(rtBin)))
-
+    elif (func3 == "1000" or func3 == "1001") or func3 == "1010": # rd, rs, rt
+        output.append(getRegister(rdBin))
+        output.append(getRegister(rsBin))
+        output.append(getRegister(rtBin))
 
     else:
         print("error")
 
     return output
     
-
+# parses fields for i-type instructions    
 def parseIType(binValue):
-    print("i")
     output = []
     opcode = binVals[0:6]
     rs = binValue[6:11]
@@ -177,21 +166,18 @@ def parseIType(binValue):
     immBin = binVals[16:32]
     output.append("i-type")
     output.append("hello-world")
-
     return output
 
+# parses fields for j-type instructions
 def parseJType(binValue):
-    print("j")
     output = []
     opcode = binValue[0:6]
     labelBin = binValue[6:32]
-    
     opname = jType[int(opcode[5:6])]
     output.append(opname)
 
     labelBin = "00" + labelBin
     label = "0x"
-    #print(len(label))
     numHex = len(labelBin) / 4
     for i in range(int(numHex)):
         label += b2h(labelBin[(4*i):(4*(i+1))])
@@ -199,23 +185,21 @@ def parseJType(binValue):
     output.append(label)
     return output
 
-
-
-
+# converts hex instruction to binary and parses based on instruction type
 def parseInput(hex):
     instrType = getType(hex[0:2])
-
     convBinary = ""
     for i in range(len(hex)):
         convBinary += h2b(hex[i:(i+1)])
 
-
     # split binary based on type 
     if instrType == 0: 
         output = parseRType(convBinary)
+
     elif instrType == 1: 
         print("j")
         output = parseJType(convBinary)
+
     else: 
         print("i")
         output = parseIType(convBinary)
@@ -224,23 +208,14 @@ def parseInput(hex):
     for i in range(len(output)):
         if i == 0 or i == (len(output) - 1): # first or last object
             mips += output[i] + " "
+
         else: # objects in middle
             mips += output[i] + ", "
+
     return mips
-
     #match instrType: use this once all issues with 3.10 are resolved
-     #   case 0: print("r")
-      #  case 1: print("j")
-
-
-def ash(ccday, w, mNum):
-    return ((((mNum- 1)*124+ (ccday + w)) - 1) % 9) + 1
-#print(ash(14, 32, 2))
-
-def ems(day,month):
-    return ((month - 1)*124 + day)
-#print(ems(46,2))
-
+    #   case 0: print("r")
+    #  case 1: print("j")
 
 def main():
     while(True):
@@ -253,22 +228,23 @@ def main():
         userInput = input() 
         try:
             hexData = validInput(userInput)
+
         except TypeError:
             print("Input needs to be a string.")
+
         except ValueError:
             print("Input needs to be 10 characters, and start with \"0x\"")
+
         except NameError:
             print("Hex value can only contain valid hexadecimal characters.")
+
         else:
             break
+
         finally:
             print("\n")
-    print(hexData)
+
     print(parseInput(hexData))
-
-
-
-
 
 if __name__ == "__main__":
     main()
