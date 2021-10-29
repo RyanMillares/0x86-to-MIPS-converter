8eb0d41eccfeb44c1493e34810cc97c271285ff0
RyanMillares <rmillares@chapman.edu>
2021-10-29 11:36:02 -0700

    edited css and layout

--- UIFunctions.js
+++ UIFunctions.js
@@ -6,3 +6,7 @@ import {parseInput} from './hexToMips.js'
 //    document.getElementById("output").value = parseInput("01234567");
 //    console.log(parseInput("01234567"));
 //}
+function swap() {
+    alert("Currently, you can only convert Instructions from Hex to readable MIPS format, not the other way around. This functionality will be developed soon!")
+}
+document.getElementById("swap").addEventListener("click", swap, false);
