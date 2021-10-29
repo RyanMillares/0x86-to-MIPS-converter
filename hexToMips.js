b40c7ff4eef23ec6e8876c39a5e71f29fc202243
RyanMillares <rmillares@chapman.edu>
2021-10-28 18:09:42 -0700

    changed a let to var

--- hexToMips.js
+++ hexToMips.js
@@ -151,7 +151,7 @@ function parseRType(binValue){
     output.push(opname)
 
     // Fetch order of fields and error handle
-    let func3 = funcBin.slice(0,4)
+    var func3 = funcBin.slice(0,4)
 
     if (func3 === "0000") { // rd, rt, sa
         output.push(getRegister(rdBin))
